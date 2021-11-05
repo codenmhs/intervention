62540e20d7f421837fde36a404790ca1e2f1835a
Wyatt Homola <whomola@cvsu.org>
2021-11-05 10:16:13 -0400

    Provides simplified answers for the whole Tex text of the fractions assignment

--- answers.py
+++ answers.py
@@ -4,18 +4,6 @@ import sympy
 from sympy.parsing.latex import parse_latex as pl
 import re
 
-test_exprns = '''\item \(\ddfrac{3}{4} * \ddfrac{1}{2} = \) \vspace{4em}
-    \item \(\ddfrac{5}{7} * \ddfrac{1}{5} = \) \vspace{4em}
-    \item \(\ddfrac{20}{3} * \ddfrac{6}{12} = \) \vspace{4em}
-    \item \(\ddfrac{-1}{3} * \ddfrac{-9}{2} = \) \vspace{4em}
-    \item \(\ddfrac{7}{3} * \ddfrac{-9}{-2} = \) \vspace{4em}
-    \item \(\ddfrac{-1}{3} * \ddfrac{52}{6} = \) \vspace{4em}
-    \item \(\ddfrac{3}{10} * \ddfrac{3}{11} = \) \vspace{4em}
-    \item \(\ddfrac{-4}{3} * \ddfrac{3}{5} = \) \vspace{4em}
-    \item \(\ddfrac{-4}{-5} * \ddfrac{-3}{7} = \) \vspace{4em}
-    \item \(\ddfrac{1}{99} * \ddfrac{11}{2} = \) \vspace{4em}
-    \item \(\ddfrac{-7}{3} * \ddfrac{15}{9} = \) \vspace{4em}'''
-
 class Expression: 
     def __init__(self, string):
         self.string = string
@@ -24,16 +12,37 @@ class Expression:
     
     @classmethod
     def from_tex(cls, tex_string): 
+        # Return an Expression object given a Tex expression
         return cls(cls.text_to_sympy(tex_string))
         
     @staticmethod
     def tex_to_sympy(tex_string): 
         return pl(tex_string)
     
+    @classmethod
+    def exprns_from_file(cls, file): 
+        '''Given a Tex file, return an array of Expression objects for the expressions in the file.'''
+        
+        exprns = []
+        # substrings matching this pattern are Tex math expressions
+        exprn_pattern = re.compile(r'\\\(.*\\\)')
+        start = re.compile(r'\\\(')
+        end = re.compile(r'=?\s?\\\)')
+        # sympy cannot parse ddfrac, so replace it with frac
+        ddfrac = re.compile('d?dfrac')
+        with open(file, 'r') as file: 
+            tex_text = file.read()
+            for item in exprn_pattern.findall(tex_text): 
+                item = start.sub('', item)
+                item = end.sub('', item)
+                item = ddfrac.sub('frac', item)
+                exprns.append(cls(item))
+        return exprns
+        
     def __repr__(self): 
         return str(self.string)
         
-    def reduce(self): 
+    def reduce(self):
         return sympy.Rational(sympy.simplify(self.string))
 
     
@@ -42,13 +51,14 @@ if __name__ == '__main__':
     # ex = Expression("\\frac{d}{dx} x^{2}")
     # ex = Expression("\\frac{256}{512}")
     # print(ex.reduce())
-    
-    # \(, followed by any number of any character, followed by \)
-    exprn = re.compile(r'\\\(.*\\\)')
-    stend = re.compile(r'(\\\()|(=.*\\\))')
-    ddfrac = re.compile('ddfrac')
-    for item in exprn.findall(test_exprns): 
-        item = stend.sub('', item)
-        item = ddfrac.sub('frac', item)
-        sy_exp = Expression(item)
-        print(str(item) + str(sy_exp.reduce()))
+
+    for item in Expression.exprns_from_file('sample.txt'):
+        simplified = ''
+        if not re.search('[a-zA-Z]', str(item)): 
+            # If there are no variables in the expression, reduce it
+            simplified = str(item.reduce())
+        print(str(item) + ' = ' + simplified)
+   
+# A simpler Tex test string than the entire file   
+test_exprns = '''\item \(\ddfrac{3}{4} * \ddfrac{1}{2} = \) \vspace{4em}
+    \item \(\ddfrac{5}{7} * \ddfrac{1}{5} = \) \vspace{4em}'''
